43abb869708e53bcbfc5628b53ad2ea876096956
Min Yu Low <lowminyu@gmail.com>
2022-10-09 04:29:05 +0800

    updated preprocessing.py

--- pipeline/parameters.py
+++ pipeline/parameters.py
@@ -2,156 +2,25 @@ import pandas as pd
 import numpy as np
 
 # features - model
-model_features_list=['membership_card_id','transaction_date','country', 'gender',
-                        'avg_items_bought_prestige_diamond',
-                        'avg_items_bought_loyal_t',
-                        'avg_items_bought_prestige_ruby',
-                        'avg_items_bought_jade',
-                        'avg_items_bought_non_member', 
-                        'avg_prestige_diamond_total_amount_spent',
-                        'avg_loyal_t_total_amount_spent',
-                        'avg_prestige_ruby_total_amount_spent', 
-                        'avg_jade_total_amount_spent',
-                        'avg_non_member_total_amount_spent',
-                        'customer_most_spent_prod_cat',
-                        'customer_most_spent_brand',
-                        'customer_most_spent_merchandise_grp',
-                        'customer_most_popular_brand',
-                        'customer_most_popular_merch_group',
-                        'customer_most_popular_prod_cat',
-                        'customer_average_discount_amount',
-                        'customer_average_discount_percentage',
-                        'customer_minimum_discount_amount',
-                        'customer_maximum_discount_amount',
-                        'customer_minimum_discount_percentage',
-                        'customer_maximum_discount_percentage',
-                        'sa_items_sold_last_30_days',
-                        'sa_sales_avg',
-                        'sa_repeaters', 
-                        'sa_repeaters_percentage', 
-                        'sa_prestige_diamond_members_percentage',
-                        'sa_loyal_t_members_percentage', 
-                        'sa_prestige_ruby_members_percentage',
-                        'sa_jade_members_percentage',
-                        'sa_non_members_percentage',
-                        'sa_very_high_spenders_percentage', 
-                        'sa_high_spenders_percentage',
-                        'sa_medium_spenders_percentage', 
-                        'sa_low_spenders_percentage',
-                        'customer_curr_total_spent_usd',
-                        'customer_curr_most_expensive',
-                        'customer_curr_item_qty',
-                        'customer_curr_spent_wja',
-                        'customer_curr_spent_fashion',
-                        'customer_curr_spent_beauty', 
-                        'customer_curr_spent_swt',
-                        'customer_curr_item_qty_wja', 
-                        'customer_curr_item_qty_fashion',
-                        'customer_curr_item_qty_beauty',
-                        'customer_curr_item_qty_swt',
-                        'customer_relative_purchasing_capacity_beauty',
-                        'customer_relative_purchasing_capacity_wja',
-                        'customer_relative_purchasing_capacity_fashion',
-                        'customer_relative_purchasing_capacity_swt',
-                        'target']
-
-
-non_repeaters_features_list=['membership_card_id','transaction_date','country', 'gender',
-                        'avg_items_bought_prestige_diamond',
-                        'avg_items_bought_loyal_t',
-                        'avg_items_bought_prestige_ruby',
-                        'avg_items_bought_jade',
-                        'avg_items_bought_non_member', 
-                        'avg_prestige_diamond_total_amount_spent',
-                        'avg_loyal_t_total_amount_spent',
-                        'avg_prestige_ruby_total_amount_spent', 
-                        'avg_jade_total_amount_spent',
-                        'avg_non_member_total_amount_spent',
-                        'customer_most_spent_prod_cat',
-                        'customer_most_spent_brand',
-                        'customer_most_spent_merchandise_grp',
-                        'customer_most_popular_brand',
-                        'customer_most_popular_merch_group',
-                        'customer_most_popular_prod_cat',
-                        'customer_average_discount_amount',
-                        'customer_average_discount_percentage',
-                        'customer_minimum_discount_amount',
-                        'customer_maximum_discount_amount',
-                        'customer_minimum_discount_percentage',
-                        'customer_maximum_discount_percentage',
-                        'sa_items_sold_last_30_days',
-                        'sa_sales_avg',
-                        'sa_repeaters', 
-                        'sa_repeaters_percentage', 
-                        'sa_prestige_diamond_members_percentage',
-                        'sa_loyal_t_members_percentage', 
-                        'sa_prestige_ruby_members_percentage',
-                        'sa_jade_members_percentage',
-                        'sa_non_members_percentage',
-                        'sa_very_high_spenders_percentage', 
-                        'sa_high_spenders_percentage',
-                        'sa_medium_spenders_percentage', 
-                        'sa_low_spenders_percentage',
-                        'customer_curr_total_spent_usd',
-                        'customer_curr_most_expensive',
-                        'customer_curr_item_qty',
-                        'customer_curr_spent_wja',
-                        'customer_curr_spent_fashion',
-                        'customer_curr_spent_beauty', 
-                        'customer_curr_spent_swt',
-                        'customer_curr_item_qty_wja', 
-                        'customer_curr_item_qty_fashion',
-                        'customer_curr_item_qty_beauty',
-                        'customer_curr_item_qty_swt',
-                        'customer_relative_purchasing_capacity_beauty',
-                        'customer_relative_purchasing_capacity_wja',
-                        'customer_relative_purchasing_capacity_fashion',
-                        'customer_relative_purchasing_capacity_swt',
-                        ]
+model_features_list = ['transcript', 'position', 'nucleotides', 'dwelling_time', 'std', 'mean', 'label']
 
 # features - preprocessing
-key_columns = ['country'] # columns that cannot be null
-
-non_nan_cols = ['avg_prestige_diamond_total_amount_spent', 'avg_prestige_ruby_total_amount_spent', # columns that have null values that should be converted to 0
-                   'avg_jade_total_amount_spent', 'avg_non_member_total_amount_spent',
-                   'avg_items_bought_prestige_diamond', 'avg_items_bought_prestige_ruby', 
-                   'avg_items_bought_jade','avg_items_bought_loyal_t','avg_items_bought_non_member', 'customer_curr_spent_fashion',
-                   'customer_curr_spent_wja', 'customer_curr_spent_swt', 'customer_curr_spent_beauty',
-                   'customer_relative_purchasing_capacity_beauty', 'customer_relative_purchasing_capacity_wja', 
-                   'customer_relative_purchasing_capacity_fashion', 'customer_relative_purchasing_capacity_swt',
-                   'customer_curr_item_qty_wja','customer_curr_item_qty_fashion', 'customer_curr_item_qty_swt', 'customer_curr_item_qty_beauty',
-                   'customer_average_discount_amount','customer_average_discount_percentage', 'customer_minimum_discount_amount',
-                   'customer_maximum_discount_amount', 'customer_minimum_discount_percentage','customer_maximum_discount_percentage']
+key_columns = ['label'] # columns that cannot be null
+non_nan_cols = []
 
 # features - encoding
-target_encode_cols = ['country', 'customer_most_spent_prod_cat', 'customer_most_spent_brand','customer_most_popular_brand','customer_most_popular_prod_cat']
-one_hot_encode_cols = ['gender', 'customer_most_spent_merchandise_grp', 'customer_most_popular_merch_group']
-target_col = 'target'
-numeric_cols = ['avg_items_bought_prestige_diamond','avg_items_bought_loyal_t','avg_items_bought_prestige_ruby',
-                           'avg_items_bought_jade', 'avg_items_bought_non_member','avg_prestige_diamond_total_amount_spent', 
-                           'avg_loyal_t_total_amount_spent', 'avg_prestige_ruby_total_amount_spent', 'avg_jade_total_amount_spent', 
-                           'avg_non_member_total_amount_spent', 'sa_items_sold_last_30_days', 'sa_sales_avg', 'sa_repeaters', 
-                           'sa_repeaters_percentage', 'sa_prestige_diamond_members_percentage', 'sa_loyal_t_members_percentage',
-                           'sa_prestige_ruby_members_percentage', 'sa_jade_members_percentage', 'sa_non_members_percentage', 
-                           'sa_very_high_spenders_percentage', 'sa_high_spenders_percentage', 'sa_medium_spenders_percentage',
-                           'sa_low_spenders_percentage', 'customer_curr_total_spent_usd', 'customer_curr_most_expensive', 'customer_curr_item_qty',
-                           'customer_curr_spent_wja', 'customer_curr_spent_fashion', 'customer_curr_spent_beauty',
-                           'customer_curr_spent_swt', 'customer_curr_item_qty_wja', 'customer_curr_item_qty_fashion', 
-                           'customer_curr_item_qty_beauty', 'customer_curr_item_qty_swt',
-                           'customer_relative_purchasing_capacity_beauty', 'customer_relative_purchasing_capacity_wja',
-                           'customer_relative_purchasing_capacity_fashion', 'customer_relative_purchasing_capacity_swt',
-                           'customer_average_discount_amount','customer_average_discount_percentage',
-                           'customer_minimum_discount_amount', 'customer_maximum_discount_amount',
-                           'customer_minimum_discount_percentage','customer_maximum_discount_percentage']
+target_encode_cols = ['nucleotides']
+target_col = 'label'
+numeric_cols = ['dwelling_time', 'std', 'mean']
 
 # oversampling / undersampling
 undersampling_strategy = 0.15
 oversampling_strategy = 0.20
 
 # data splitting
-stratify_col = 'target' # column used to stratify data
+stratify_col = 'label' # column used to stratify data
 seed = 42 # random state
-id_col = 'membership_card_id'
+id_col = ['transcript', 'position']
     # the following 3 percentages should sum to 1
 train_percent = 0.8 # percentage of train data
 validation_percent = 0.1 # percentage of validation data
--- pipeline/preprocessing/preprocessing.py
+++ pipeline/preprocessing/preprocessing.py
@@ -3,25 +3,34 @@ import numpy as np
 
 from parameters import (model_features_list, key_columns, non_nan_cols, stratify_col, 
                         train_percent, validation_percent, test_percent, seed, id_col, 
-                        target_encode_cols, one_hot_encode_cols, numeric_cols, target_col,
+                        target_encode_cols, numeric_cols, target_col,
                         undersampling_strategy, oversampling_strategy)
-from utilities import get_first_transaction
 
 from sklearn.compose import ColumnTransformer
 from sklearn.pipeline import Pipeline
-from sklearn.preprocessing import MinMaxScaler, OneHotEncoder
+from sklearn.preprocessing import MinMaxScaler
 from sklearn.model_selection import train_test_split
 from imblearn.over_sampling import SMOTE
 from imblearn.under_sampling import RandomUnderSampler
-from category_encoders import OneHotEncoder, TargetEncoder
+from category_encoders import TargetEncoder
 
 
 class Preprocessing:
     def __init__(self, df):
         self.df = df
 
-        self.columns_to_map = []
-
+        self.columns_to_map = None
+        self.df_train = None
+        self.df_test = None
+        self.df_val = None
+        self.X_train = None
+        self.y_train = None
+        self.X_val = None
+        self.y_val = None
+        self.X_test = None
+        self.y_test = None
+        self.pipe = None
+        
     def model_features_and_clean(self):
         '''Function to select features for modelling and clean the initial raw data. 
             Removes rows which contain NAN values in key_columns, and converts all NAN values to 0 for non_nan_cols
@@ -51,10 +60,8 @@ class Preprocessing:
 
         :Returns:
         ---------
-            df_train_all_tx: DataFrame 
-                DataFrame containing training features and target for all transactions
-            df_train_first_tx:  DataFrame
-                DataFrame containing training features and target for first transaction
+            df_train: DataFrame 
+                DataFrame containing training features and target
             df_test:   DataFrame
                 DataFrame containing test features and target 
             df_val:    DataFrame
@@ -63,10 +70,6 @@ class Preprocessing:
                 DataFrame containing training features
             y_train:   DataFrame
                 DataFrame containing target variable for training data
-            X_train_first_tx: DataFrame
-                DataFrame containing training features for training first transaction
-            y_train_first_tx: DataFrame
-                DataFrame containing target variable for training first transaction
             X_val: DataFrame
                 DataFrame containing validation features
             y_val: DataFrame
@@ -76,10 +79,16 @@ class Preprocessing:
             y_test: DataFrame
                 DataFrame containing target variable for test data
             df_val_id: DataFrame
-                DataFrame containing validation features and target, including membership_card_id
-            list_train_members: List
-                List containing membership_card_id for training 
+                DataFrame containing validation features and target, including id_cols
+            list_train: List
+                List containing id_cols for training 
         '''
+        # getting df for splitting
+        temp_col = id_col + [stratify_col]
+        df_target = self.df[temp_col].drop_duplicates()
+        X = df_target  # Contains all columns.
+        # Dataframe of just the column on which to stratify.
+        y = df_target[[stratify_col]]
 
         # parameter checks
         if train_percent + validation_percent + test_percent != 1.0:
@@ -90,76 +99,67 @@ class Preprocessing:
             raise ValueError('%s is not a column in the dataframe' %
                             (stratify_col))
 
-        # getting df for splitting
-        df_target = self.df[[id_col, stratify_col]].drop_duplicates()
-        X = df_target  # Contains all columns.
-        # Dataframe of just the column on which to stratify.
-        y = df_target[[stratify_col]]
 
         # Split original dataframe into train and temp dataframes.
-        df_train, df_temp, y_train, y_temp = train_test_split(X, y, stratify=y,
+        self.df_train, df_temp, self.y_train, y_temp = train_test_split(X, y, stratify=y,
                                                                 test_size=(1.0 - train_percent),
                                                                 random_state=random_state)
 
         # Split the temp dataframe into val and test dataframes.
         relative_test_percent = test_percent / (validation_percent + test_percent)
-        df_val, df_test, y_val, y_test = train_test_split(df_temp, y_temp,
+        self.df_val, self.df_test, self.y_val, self.y_test = train_test_split(df_temp, y_temp,
                                                             stratify=y_temp,
                                                             test_size=relative_test_percent,
                                                             random_state=random_state)
 
-        assert len(df_target) == len(df_train) + len(df_val) + len(df_test)
+        assert len(df_target) == len(self.df_train) + len(self.df_val) + len(self.df_test)
 
-        # getting list of members of each df
-        list_train_members = np.array(df_train[id_col])
-        list_test_members = np.array(df_test[id_col])
-        list_val_members = np.array(df_val[id_col])
+        # getting list of id_cols of each df
+        list_train = np.array(self.df_train[id_col])
+        list_train = [tuple(i) for i in list_train]
+        list_test = np.array(self.df_test[id_col])
+        list_test = [tuple(i) for i in list_test]
+        list_val = np.array(self.df_val[id_col])
+        list_val = [tuple(i) for i in list_val]
 
-        # creating train data with all transactions, removing membership_card_id and transaction_date
-        df_train_all_tx = self.df[self.df[id_col].isin(list_train_members)]
-        df_train_all_tx.drop(columns=['transaction_date', id_col], inplace=True)
+        # creating train data with all transactions, removing id_cols
+        self.df_train = self.df[self.df[id_col].apply(tuple, axis = 1).isin(list_train)]
+        self.df_train.drop(columns=id_col, inplace=True)
+        self.df_test = self.df[self.df[id_col].apply(tuple, axis = 1).isin(list_test)]
+        self.df_test.drop(columns=id_col, inplace=True)
+        self.df_val = self.df[self.df[id_col].apply(tuple, axis = 1).isin(list_val)]
+        df_val_id = self.df_val.copy()
+        self.df_val.drop(columns=id_col, inplace=True)
 
-        # get df with first transaction
-        df_train_first_tx = get_first_transaction(self.df, list_train_members)
-        df_test = get_first_transaction(self.df, list_test_members)
-        df_val, df_val_id = get_first_transaction(self.df, list_val_members, with_id = True)
 
         # printing percentages
-        print("train percentage:", len(df_train_all_tx[df_train_all_tx[target_col] == 1])/len(df_train_all_tx))
-        print("train percentage first timers:", len(df_train_first_tx[df_train_first_tx[target_col] == 1])/len(df_train_first_tx))
-        print("test percentage:", len(df_test[df_test[target_col] == 1])/len(df_test))
-        print("val percentage:", len(df_val[df_val[target_col] == 1])/len(df_val))
+        print("train percentage:", len(self.df_train[self.df_train[target_col] == '1'])/len(self.df_train))
+        print("test percentage:", len(self.df_test[self.df_test[target_col] == '1'])/len(self.df_test))
+        print("val percentage:", len(self.df_val[self.df_val[target_col] == '1'])/len(self.df_val))
 
         # printing df shape
-        print("train data shape:", df_train_all_tx.shape)
-        print("train data first timers shape:", df_train_first_tx.shape)
-        print("validation data shape:", df_val.shape)
-        print("test data shape:", df_test.shape)
+        print("train data shape:", self.df_train.shape)
+        print("validation data shape:", self.df_val.shape)
+        print("test data shape:", self.df_test.shape)
 
         # separating df from target column: features -> X | target -> y
-        X_train = df_train_all_tx.drop(columns=target_col)
-        y_train = pd.DataFrame(df_train_all_tx[target_col])
-        X_train_first_tx = df_train_first_tx.drop(columns=target_col)
-        y_train_first_tx = pd.DataFrame(df_train_first_tx[target_col])
-        X_val = df_val.drop(columns=target_col)
-        y_val = pd.DataFrame(df_val[target_col])
-        X_test = df_test.drop(columns=target_col)
-        y_test = pd.DataFrame(df_test[target_col])
+        self.X_train = self.df_train.drop(columns=target_col)
+        self.y_train = pd.DataFrame(self.df_train[target_col])
+        self.X_val = self.df_val.drop(columns=target_col)
+        self.y_val = pd.DataFrame(self.df_val[target_col])
+        self.X_test = self.df_test.drop(columns=target_col)
+        self.y_test = pd.DataFrame(self.df_test[target_col])
 
-        return(df_train_all_tx, df_train_first_tx, df_test, df_val, X_train, y_train, X_train_first_tx,
-            y_train_first_tx, X_val, y_val, X_test, y_test, df_val_id, list_train_members)
+        return(self.df_train, self.df_test, self.df_val, self.X_train, self.y_train,
+                self.X_val, self.y_val, self.X_test, self.y_test, df_val_id, list_train)
 
 
-    def encoding_train(self, x_df, y_df):
-        '''Function to encode categorical features for training data. 
-            Features with fewer than 10 unique variables will be one hot encoded, and the rest will be target encoded
+    def encoding_train(self):
+        '''Function to encode categorical features for training data.
 
         :Parameters:
         -----------
-            x_df: DataFrame
-                DataFrame containing raw features
-            y_df: DataFrame
-                Dataframe containing target column
+
         :Returns:
         ---------
             x_df: DataFrame
@@ -170,51 +170,35 @@ class Preprocessing:
 
         # piping the encoding
         numeric_encoder = Pipeline([('scale', MinMaxScaler())])
-        one_hot_encoder = Pipeline([('one_hot', OneHotEncoder(handle_unknown='ignore'))])
         target_encoder = Pipeline([('target_encode', 
                                         TargetEncoder(cols = target_encode_cols, 
                                         handle_unknown='return_nan'))])
         preprocessor = ColumnTransformer(transformers = [
                                             ("num", numeric_encoder, numeric_cols),
-                                            ("target_encode", target_encoder, target_encode_cols), ("one_hot_encode", one_hot_encoder, one_hot_encode_cols)], 
+                                            ("target_encode", target_encoder, target_encode_cols)], 
                                             remainder = 'passthrough')
 
         # getting list of column names to map
         self.columns_to_map = numeric_cols + target_encode_cols
-        one_hot = OneHotEncoder(handle_unknown='ignore',use_cat_names=True)
-        x_df_ohe = pd.DataFrame(one_hot.fit_transform(x_df[one_hot_encode_cols]))
-        x_df_ohe.columns = one_hot.get_feature_names()
-        self.columns_to_map = numeric_cols + target_encode_cols + list(x_df_ohe.columns)
 
-        # for f in one_hot_encode_cols:
-        #     cols = pd.get_dummies(x_df[f]).columns
-        #     columns_to_map.extend(f + '_' + str(col)
-        #                                 for col in cols)
-
-        # columns_to_map.extend('gender_' + str(col)
-        #                             for col in pd.get_dummies(x_df['gender']).columns)
-        # columns_to_map.extend('customer_most_spent_merchandise_grp_' + str(col)
-        #                             for col in pd.get_dummies(x_df['customer_most_spent_merchandise_grp']).columns)
         print('columns after preprocessing :', self.columns_to_map,  '\n')
 
         # applying encoding on columns in df and creating pipeline
-        x_df = pd.DataFrame({col: vals for vals, col in zip(preprocessor.transform(x_df, y_df).T, self.columns_to_map)})
-        pipe = Pipeline(steps=[("preprocessor", preprocessor)])
-        pipe = pipe.fit(x_df, y_df)
+        self.X_train = pd.DataFrame({col: vals for vals, col in zip(preprocessor.fit_transform(self.X_train, self.y_train).T, self.columns_to_map)})
+        self.pipe = Pipeline(steps=[("preprocessor", preprocessor)])
+        self.pipe = self.pipe.fit(self.X_train, self.y_train)
 
-        return x_df, pipe
+        return self.X_train, self.pipe
     
 
-    def encoding_test_val(self, x_input, pipe):
+    def encoding_test_val(self, test = True):
         '''Function to encode categorical features for test and validation data. 
-            Features with fewer than 10 unique variables will be one hot encoded, and the rest will be target encoded
 
         :Parameters:
         -----------
-            y_input: DataFrame
-                Dataframe containing test/validation features
-            pipe: Pipeline
-                Pipeline created from training data
+            test: Boolean
+                if test = True, encode for X_test. if False, encode for X_val
+
         :Returns:
         ---------
             x_output: DataFrame
@@ -222,36 +206,39 @@ class Preprocessing:
         '''
 
         # applying encoding on columns in df
-        x_output = pd.DataFrame({col:vals for vals,col in zip(pipe.transform(x_input).T, self.columns_to_map)})
-        return x_output
+            # for test
+        if test == True:
+            self.X_test = pd.DataFrame({col:vals for vals,col in zip(self.pipe.transform(self.X_test).T, self.columns_to_map)})
+            return self.X_test
+            # for validation
+        else:
+            self.X_val = pd.DataFrame({col:vals for vals,col in zip(self.pipe.transform(self.X_val).T, self.columns_to_map)})
+            return self.X_val
+        
         
 
-    def oversample_undersample(X_train_first_timers, y_train_first_timers, sampling=True):
+    def oversample_undersample(x_df, y_df, sampling=True):
         '''Function to oversample minority and undersample majority.
 
         :Parameters:
         ------------
-            X_train_first_timers: DataFrame
+            x_df: DataFrame
                 DataFrame containing training data only for first purchase date excluding the target variable 
-            y_train_first_timers: DataFrame
+            y_df: DataFrame
                 DataFrame containing target variable for training data only for first purchase date
 
         :Returns:
         ---------
-            X_train_first_timers: DataFrame
+            x_df: DataFrame
                 DataFrame contiaining oversampled and understampled training data only for first purchase date excluding target variable
-            y_train_first_timers: DataFrame
+            y_df: DataFrame
                 DataFrame containing oversampled and undersampled target variable for training data only for first purchase date
 
         '''
 
         if sampling:
-            undersample = RandomUnderSampler(
-                sampling_strategy = undersampling_strategy, random_state=seed)
-            oversample = SMOTE(
-                sampling_strategy = oversampling_strategy, random_state=seed)
-            X_train_first_timers, y_train_first_timers = undersample.fit_resample(
-                X_train_first_timers, y_train_first_timers)
-            X_train_first_timers, y_train_first_timers = oversample.fit_resample(
-                X_train_first_timers, y_train_first_timers)
-        return X_train_first_timers, y_train_first_timers
+            undersample = RandomUnderSampler(sampling_strategy = undersampling_strategy, random_state=seed)
+            oversample = SMOTE(sampling_strategy = oversampling_strategy, random_state=seed)
+            x_df, y_df = undersample.fit_resample(x_df, y_df)
+            x_df, y_df = oversample.fit_resample(x_df, y_df)
+        return x_df, y_df
