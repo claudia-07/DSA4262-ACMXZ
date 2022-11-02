d566575750ac4edcb18a6f0f5bb640f6b1ebe185
LunarSapphic <lowminyu@gmail.com>
2022-11-02 23:48:43 +0800

    "updated preprocessing to take in multiple inputs and concatonate them"

--- deployment/preprocessing.py
+++ deployment/preprocessing.py
@@ -5,22 +5,22 @@ import pickle
 
 from util.model import (parse_line)
 from util.preprocess import (Preprocessing)
-from util.model import (get_percent, Modelling)
 
-# taking in raw df from command line
-data = sys.argv
+# taking in raw df from command line and parsing them into dataframe
+n = len(sys.argv)
+data_df = pd.DataFrame()
+for i in range(1, n):
+    data = sys.argv[i]
+    reads_df = [parse_line(data[j]) for j in range(len(data))]
+    data_df = pd.concat(reads_df, axis = 0)
 
 # importing info
 with open("../data/raw_data/data.info", 'r') as f:
     info = f.read().splitlines()
 
 # importing encoding pipeline
-filename = 'encoding_pipeline.pkl'
+filename = '../data/raw_data/encoding_pipeline.pkl'
 pipe = pickle.load(open(filename, 'rb'))
-
-### parse all lines in json file into dataframe
-reads_df = [parse_line(data[i]) for i in range(len(data))]
-data_df = pd.concat(reads_df, axis = 0)
 
 # info
 info_list = [info[i].split(",") for i in range(len(info))]
@@ -33,15 +33,12 @@ df = df.drop(['transcript_id', 'transcript_position'],  axis = 1)
 df_pre = Preprocessing(df)
 df_pre.model_features_and_clean()
 df = df_pre.feature_eng()
-# splitting in df and df_id (with id columns)
-id_col = ['transcript', 'position', 'gene_id', 'label']
-# df_id = df
-df = df.drop(columns=id_col)
+
 # encoding
 for i in range(7):
     df['position_' + str(i)] = df['nucleotides'].apply(lambda x: x[i])
 df_enc = pd.DataFrame({col: vals for vals, col in zip(pipe.transform(df).T, df_pre.columns_to_map)})
 
 # returning df
-with open('df', 'w') as sys.stdout:
+with open('df.csv', 'w') as sys.stdout:
     print(df)
