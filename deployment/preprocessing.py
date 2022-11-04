fc134a7da0d59fa094ff9fe6f1b7c8618b11acf2
LunarSapphic <lowminyu@gmail.com>
2022-11-04 12:50:06 +0800

    "debugging preprocessing"

--- deployment/preprocessing.py
+++ deployment/preprocessing.py
@@ -16,22 +16,18 @@ n = len(sys.argv)
 data_df = pd.DataFrame()
 for i in range(1, n):
     data = [json.loads(line) for line in open(sys.argv[i], 'r')]
-    print(type(data))
-    print(len(data))
     reads_df = [parse_line(data[j]) for j in range(len(data))]
     data_df = pd.concat(reads_df, axis = 0)
-    print("parsed " + str(n-1) + " number of files")
-print("parsed all data")
-print(data_df.columns)
 
 # importing info
 with open("data/raw_data/data.info", 'r') as f:
     info = f.read().splitlines()
 
-# importing encoding pipeline
+# importing encoding pipeline and columns for mapping
 filename = 'data/raw_data/encoding_pipeline.pkl'
 pipe = pickle.load(open(filename, 'rb'))
-print("imported all files")
+filename = 'data/raw_data/columns.pkl'
+columns = pickle.load(open(filename, 'rb'))
 
 # info
 info_list = [info[i].split(",") for i in range(len(info))]
@@ -39,19 +35,16 @@ info_df = pd.DataFrame(info_list[1:])
 info_df.columns = info_list[0]
 df = data_df.merge(info_df, how = "left", left_on = ["transcript", "position"], right_on = ["transcript_id", "transcript_position"])
 df = df.drop(['transcript_id', 'transcript_position'],  axis = 1)
-print("merged data with info")
 
 ### preprocessing
 df_pre = Preprocessing(df)
 df_pre.model_features_and_clean()
 df = df_pre.feature_eng()
-print("data cleaned and feature engineering completed")
 
 # encoding
 for i in range(7):
     df['position_' + str(i)] = df['nucleotides'].apply(lambda x: x[i])
-df_enc = pd.DataFrame({col: vals for vals, col in zip(pipe.transform(df).T, df_pre.columns_to_map)})
-print("encoding completed")
+df_enc = pd.DataFrame({col: vals for vals, col in zip(pipe.transform(df).T, columns)})
 
 # returning df
 with open('df.csv', 'w') as sys.stdout:
